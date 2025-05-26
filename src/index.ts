89ca4b1af8448988409ceaa61f50354526f9d8b5
amird308 <amird308@gmail.com>
2025-05-26 16:54:28 -0700

    style: implement default input style

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@
 import "./styles/globals.css";
 
 // Export components
-export { Input } from "./components/Input";
+export { Input } from "./components/input/Input";
 
-export type { InputProps } from "./components/Input";
+export type { InputProps } from "./components/input/Input";
