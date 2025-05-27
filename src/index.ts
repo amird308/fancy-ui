195277977b8d4f23013bdcd7b9018f343f0ddef7
amird308 <amird308@gmail.com>
2025-05-26 18:24:10 -0700

    fix: export other components

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,26 @@
 import "./styles/globals.css";
 
 // Export components
+export { Button, buttonVariants } from "./components/button/Button";
 export { Input } from "./components/input/Input";
+export {
+  Modal,
+  ModalPortal,
+  ModalOverlay,
+  ModalTrigger,
+  ModalClose,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalTitle,
+  ModalDescription,
+} from "./components/modal/Modal";
+export { DatePicker } from "./components/date-picker/DatePicker";
 
+// Export utilities
+export { cn } from "./lib/utils";
+
+// Export types
+export type { ButtonProps } from "./components/button/Button";
 export type { InputProps } from "./components/input/Input";
+export type { DatePickerProps } from "./components/date-picker/DatePicker"; 
